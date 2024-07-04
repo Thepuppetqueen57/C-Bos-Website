0db393607a3004bd3be09386edea3e6e5c1fa059
Puppet <122136792+Thepuppetqueen57@users.noreply.github.com>
2024-07-03 17:16:39 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Buttons from "./lib/Buttons.svelte";
+</script>
+  
+<main>
+  <div class="container">
+    <h1>C-Bos</h1>
+    <Buttons />
+  </div>
+</main>
+  
+<style lang="scss">
+  $purple: #370664;
+  $purpletext: #420068;
+  $lightgray: #b6b6b6;
+  
+  main {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: Arial, sans-serif;
+  }
+  
+  .container {
+    text-align: center;
+  }
+  
+  h1 {
+    margin-bottom: 20px;
+    color: $purpletext;
+  }
+</style>
---
+++ src/lib/Buttons.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    function redirect(url: string) {
+        window.location.href = url
+    }
+
+    const infourl = "https://thepuppet57.141412.xyz/tps/cbos/info.html"
+    const packagelisturl = "https://thepuppet57.141412.xyz/tps/cbos/packagelist"
+    const repourl = "https://github.com/Thepuppetqueen57/C-Bos"
+    const backendurl = "https://thepuppet57.141412.xyz/tps/cbos/backend"
+</script>
+
+<main>
+    <div class="button-container">
+        <button on:click={(event) => redirect(infourl)}>Info</button>
+        <button on:click={(event) => redirect(packagelisturl)}>Package List</button>
+        <button on:click={(event) => redirect(repourl)}>Source Code</button>
+        <button on:click={(event) => redirect(backendurl)}>Backend</button>
+    </div>
+</main>
+
+<style lang="scss">
+  $purple: #370664;
+  $purpletext: #420068;
+  $lightgray: #b6b6b6;
+
+  .button-container {
+    display: flex;
+    gap: 5px;
+  }
+
+  button {
+    background-color: $purple;
+    color: $lightgray;
+    width: 90px;
+    height: 40px;
+    border-radius: 10px;
+    border: none;
+    transition: 0.3s;
+  }
+
+  button:hover {
+    transform: scale(1.1);
+    background-color: darken($purple, 3);
+  }
+</style>
